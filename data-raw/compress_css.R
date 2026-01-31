3e6b5ad82ec29176e8aaf5ed3b51168c946e83f4
Diego H. <diego.hernangomezherrero@gmail.com>
2026-01-31 20:33:04 +0100

    Refactor code using more bslib vars

--- data-raw/compress_css.R
+++ data-raw/compress_css.R
@@ -3,9 +3,13 @@ library(sass)
 
 lns <- readLines("inst/pkgdown/assets/BS5/gitdevr.css")
 
+# writeLines(lns, "inst/pkgdown/assets/BS5/gitdevr.min.css")
+
 sass(
   lns,
   "inst/pkgdown/assets/BS5/gitdevr.min.css",
   cache = NULL,
   options = sass_options(output_style = "compressed")
 )
+
+message("OK, compressed")
